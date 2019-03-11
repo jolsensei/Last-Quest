9e6627953432212a28b855ddd691f5029b1d83ce
jolsensei <horusensei@gmail.com>
2019-03-11 23:01:39 +0100

    Algo se ha hecho

---
+++ ProyectoFinal/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float Speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newMov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + newMov, Speed * Time.deltaTime);
+    }
+}
