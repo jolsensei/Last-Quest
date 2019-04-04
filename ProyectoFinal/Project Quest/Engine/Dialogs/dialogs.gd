378b98d5108579a73af931568986c7210c81b620
jolsensei <horusensei@gmail.com>
2019-04-04 21:52:48 +0200

    Noice dialogs

--- ProyectoFinal/Project Quest/Engine/Dialogs/dialogs.gd
+++ ProyectoFinal/Project Quest/Engine/Dialogs/dialogs.gd
@@ -1,18 +1,56 @@
 extends Control
 
-var is_paused = false
+var globalDialog
+var page = 0
+var currently_in_dialog = false
 
 func _ready():
 	_DIALOG_MANAGER.connect("send_dialog", self, "print_dialog")
+	$Container/Label.set_visible_characters(0)
+	set_process_input(false)
+	
 
 func print_dialog(dialog):
-	visible = true
 	
-	get_tree().paused = true
+	globalDialog = dialog
 	
-	$Container/Label.text = dialog
+	$Container/Label.set_bbcode(globalDialog[page])
 	
-	is_paused = true
+	$Timer.start()
+	
+	on_dialog(true)
+	
+	
+	
+	
 	
 
+func _input(event):
+	if Input.is_action_just_pressed("a") and currently_in_dialog:
+		if $Container/Label.get_visible_characters() > $Container/Label.get_total_character_count():
+			if page < globalDialog.size()-1:
+				page += 1
+				$Container/Label.set_bbcode(globalDialog[page])
+				$Container/Label.set_visible_characters(0)
+			else:
+				Input.action_release("a")
+				on_dialog(false)
+		else:
+			$Container/Label.set_visible_characters($Container/Label.get_total_character_count())
+
+
+func _on_Timer_timeout():
+	$Container/Label.set_visible_characters($Container/Label.get_visible_characters()+1)
+	
+func on_dialog(boolean):
+	
+	currently_in_dialog = boolean
+	set_process_input(boolean)
+	get_tree().paused = boolean
+	visible = boolean
+	
+	if !boolean:
+		$Container/Label.set_visible_characters(0)
+		$Timer.stop()
+		page = 0
 
--- ProyectoFinal/Project Quest/Events/Sign/sign.gd
+++ ProyectoFinal/Project Quest/Events/Sign/sign.gd
@@ -1,19 +1,26 @@
 extends Node2D
 
+export(String) var text
 var can_read = false
 
+func _ready():
+	set_process_input(false) #This way _input is not called in every key input
 
 func _on_Area2D_body_entered(body):
 	if body.get("type") == _ENUMS.TYPE.PLAYER:
-		print("Near")
-		_GLOBAL_DATA.player.can_read = true
+		close_to(true)
 
 
 func _on_Area2D_body_exited(body):
 	if body.get("type") == _ENUMS.TYPE.PLAYER:
-		print("Far")
-		_GLOBAL_DATA.player.can_read = false
+		close_to(false)
+		
+func close_to(close):
+		print("Close") if close else print("Far")
+		_GLOBAL_DATA.player.can_read = close
+		set_process_input(close)
 		
 func _input(event):
+#	print("Test")
 	if Input.is_action_just_pressed("a") and _GLOBAL_DATA.player.can_read:
-		_DIALOG_MANAGER.show("Hello, this is your first sign :D")
+		_DIALOG_MANAGER.show(["This is a test, you can press A to skip the following Lorem Ipsum so you know...", "Hope this works well"])
