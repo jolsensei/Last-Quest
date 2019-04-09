6dbb91ccd7a7dc7a3f35526945643e9ff43f0f49
jolsensei <horusensei@gmail.com>
2019-04-09 21:08:06 +0200

    Pues hay un pot, lo levantas y lo dejas

---
+++ ProyectoFinal/Project Quest/Events/Pot/pot.gd
@@ -0,0 +1,55 @@
+extends "res://Events/basic_event.gd"
+
+var picked = false
+
+func _ready():
+	set_physics_process(false)
+
+func _input(event):
+
+	if Input.is_action_just_pressed("a") and _GLOBAL_DATA.player.can_interact:
+		$Sprite/StaticBody2D/CollisionShape2D.disabled = true
+		picked = true
+		set_z_index(1)
+		set_physics_process(true)
+		
+		Input.action_release("a")
+		
+
+	if Input.is_action_just_pressed("a") and picked:
+		$Sprite/StaticBody2D/CollisionShape2D.disabled = false
+		picked = false
+		set_z_index(0)
+		set_physics_process(false)
+		
+		drop()
+		
+		Input.action_release("a")
+		
+		
+func _physics_process(delta):
+	if picked:
+		self.global_position = _GLOBAL_DATA.player.get_node("Lift").global_position
+		set_process_input(true)
+
+
+func drop():
+	var player_position = _GLOBAL_DATA.player.global_position
+	match(_GLOBAL_DATA.player.spriteMov):
+		"left":
+			player_position.x -= 14
+		"right":
+			player_position.x += 14
+		"down":
+			player_position.y += 16
+		"up":
+			player_position.y -= 4
+	self.global_position = player_position
+
+
+func _on_Area2D_body_entered(body):
+	_on_body_entered(body)
+
+
+func _on_Area2D_body_exited(body):
+	_on_body_exited(body)
